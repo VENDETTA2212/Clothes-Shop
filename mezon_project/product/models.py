012d7d0553e6f161c5c971bd56004ed55445e6c1
Bardia <111596964+VENDETTA2212@users.noreply.github.com>
2023-10-28 18:39:29 +0330

    Update models.py

--- mezon_project/product/models.py
+++ mezon_project/product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Dress(models.Model):
+    pass
+
+class Pants(models.Model):
+    pass
+
+class Hat(models.Model):
+    pass
+
+class TShirt(models.Model):
+    pass
+
+class Shirt(models.Model):
+    pass
+
+class Top(models.Model):
+    pass
+
+class Scarf(models.Model):
+    pass
+
+class Bag(models.Model):
+    pass
+
+class Glasses(models.Model):
+    pass
+
+class Jacket(models.Model):
+    pass
